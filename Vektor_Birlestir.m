79840f696c94b4b80b3aed8571517df79f0f7079
Abdulkadir Burak AFŞAR <burakafsar4066@gmail.com>
2024-11-23 23:55:23 +0300

    Add files via upload

---
+++ Vektor_Birlestir.m
@@ -0,0 +1,7 @@
+clear
+clc
+x=input("X ekseni değerini giriniz\n");
+y=input("Y ekseni değerini giriniz\n");
+z=input("Z ekseni değerini giriniz\n");
+vek=(x^2+y^2+z^2)^0.5;
+fprintf("\nVektörünüz %.2f'dir\n",vek);
